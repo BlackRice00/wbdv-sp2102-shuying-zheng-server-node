d475d205120d5c0250e3a7377698c036163c2f47
MoiraZsy <39940336+MoiraZsy@users.noreply.github.com>
2021-04-17 23:18:07 -0700

    create daos files

--- services/questions/questions-service.js
+++ services/questions/questions-service.js
@@ -1,30 +1,38 @@
-const questions = require('./questions.json');
-const questionsModel = require("../../models/questions/questions-model")
+// const questions = require('./questions.json');
+// const questionsModel = require("../../models/questions/questions-model")
+//
+// const createQuestion = () => {}
+// const createQuestionForQuiz = () => {}
+//
+// const findAllQuestions = () => {
+//     return questionsModel.find();
+// }
+// const findQuestionsForQuiz = (qzid) => {
+//     // return questions.filter((question) => {
+//     //     return question.quizId === qzid;
+//     // })
+//     return questionsModel.find({quizId: qzid});
+// }
+//
+// const findQuestionById = (quid) => {
+//     return questions.find((question) => {
+//         return question._id === quid;
+//     });
+// }
+//
+// const updateQuestion = () => {}
+// const deleteQuestion = () => {}
+//
+// module.exports = {
+//     createQuestion, findQuestionsForQuiz,
+//     findAllQuestions, findQuestionById,
+//     updateQuestion, deleteQuestion
+// }
 
-const createQuestion = () => {}
-const createQuestionForQuiz = () => {}
+const questionsDao = require('../daos/questions-dao')
+const findAllQuestions = () => questionsDao.findAllQuestions()
+const findQuestionById = (qid) => questionsDao.findQuestionById(qid)
+const findQuestionsForQuiz = (qid) => questionsDao.findQuestionsForQuiz(qid)
+module.exports = { findAllQuestions, findQuestionById, findQuestionsForQuiz }
 
-const findAllQuestions = () => {
-    return questionsModel.find();
-}
-const findQuestionsForQuiz = (qzid) => {
-    // return questions.filter((question) => {
-    //     return question.quizId === qzid;
-    // })
-    return questionsModel.find({quizId: qzid});
-}
 
-const findQuestionById = (quid) => {
-    return questions.find((question) => {
-        return question._id === quid;
-    });
-}
-
-const updateQuestion = () => {}
-const deleteQuestion = () => {}
-
-module.exports = {
-    createQuestion, findQuestionsForQuiz,
-    findAllQuestions, findQuestionById,
-    updateQuestion, deleteQuestion
-}
--- services/quizzes/quizzes-service.js
+++ services/quizzes/quizzes-service.js
@@ -1,32 +1,41 @@
-const quizzes = require('./quizzes.json')
 
-const quizzesModel = require('../../models/quizzes/quizzes-model')
+const quizzesDao = require('../daos/quizzes-dao')
+const findAllQuizzes = () => quizzesDao.findAllQuizzes()
+const findQuizById = (qid) => quizzesDao.findQuizById(qid)
+module.exports = { findAllQuizzes, findQuizById }
 
-// TODO: Node.js Assignment
-const findAllQuizzes = () => {
-    return quizzesModel.find()
-}
-const findQuizById = (quizId) => {
-    // return quizzes.find((quiz) => {
-    //     return quiz._id === quizId
-    // })
-    return quizzesModel
-        .findById(quizId)
-        .populate("quizzes")
-        .exec()
-}
-// console.log(findAllQuizzes())
-// console.log(findQuizById('123'))
 
-// TODO: MongoDB Assignment
-const createQuiz = () => {}
-const updateQuiz = () => {}
-const deleteQuiz = () => {}
 
-module.exports = {
-    createQuiz,
-    findAllQuizzes,
-    findQuizById,
-    updateQuiz,
-    deleteQuiz
-}
+
+// const quizzes = require('./quizzes.json')
+//
+// const quizzesModel = require('../../models/quizzes/quizzes-model')
+//
+// // TODO: Node.js Assignment
+// const findAllQuizzes = () => {
+//     return quizzesModel.find()
+// }
+// const findQuizById = (quizId) => {
+//     // return quizzes.find((quiz) => {
+//     //     return quiz._id === quizId
+//     // })
+//     return quizzesModel
+//         .findById(quizId)
+//         .populate("quizzes")
+//         .exec()
+// }
+// // console.log(findAllQuizzes())
+// // console.log(findQuizById('123'))
+//
+// // TODO: MongoDB Assignment
+// const createQuiz = () => {}
+// const updateQuiz = () => {}
+// const deleteQuiz = () => {}
+//
+// module.exports = {
+//     createQuiz,
+//     findAllQuizzes,
+//     findQuizById,
+//     updateQuiz,
+//     deleteQuiz
+// }
